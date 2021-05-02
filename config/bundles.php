52faf8f65dd8a8ebe2bf5b1a3569ec7bbe62c006
Yusuf <yusuf@ozdemir.be>
2021-05-02 18:25:19 +0300

    remove vich/uploader-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
