b31bf2b7981e6f02055d0e5c7fcfa0ca23f4b15f
Yusuf <yusuf@ozdemir.be>
2021-05-04 23:40:13 +0300

    dump schema

--- migrations/Version20210504203504.php
+++ migrations/Version20210504203504.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210429183252 extends AbstractMigration
+final class Version20210504203504 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,10 @@ final class Version20210429183252 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE app_users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, email VARCHAR(254) NOT NULL, is_active BOOLEAN NOT NULL, is_verified BOOLEAN NOT NULL, roles CLOB NOT NULL --(DC2Type:json_array)
+        )');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C2502824F85E0677 ON app_users (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C2502824E7927C74 ON app_users (email)');
         $this->addSql('CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, category_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE INDEX IDX_CBE5A33112469DE2 ON book (category_id)');
         $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
@@ -28,6 +32,7 @@ final class Version20210429183252 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE app_users');
         $this->addSql('DROP TABLE book');
         $this->addSql('DROP TABLE category');
     }
